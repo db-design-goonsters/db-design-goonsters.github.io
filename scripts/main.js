4f960721d16baa098e01afce9eca2cd33fc0cd45
o-cronin <79036835+o-cronin@users.noreply.github.com>
2024-07-21 16:41:26 -0400

    ary

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,19 @@
 import { pool } from "./services/pool";
 
-const clickMe = document.querySelector('.intro .test-connection');
+// const clickMe = document.querySelector('.intro .test-connection');
 
-clickMe.onclick = function testConnection() {
-    fetch('/get-suppliers')
-        .then(response => response.json())
-        .then(data => {
-            console.log('Query result:', data);
-            // Update your UI here, for example:
-            displaySuppliers(data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            // Handle errors here
-        });
-}
-
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const clickMe = document.querySelector('.intro .test-connection');
+document.addEventListener('DOMContentLoaded', () => {
+    const clickMe = document.querySelector('.intro .test-connection');
   
-//     clickMe.onclick = function testConnection() {
-//       pool.query('SELECT * FROM Supplier', (err, result) => {
-//         if (err) {
-//           console.error('Error executing query:', err);
-//         } else {
-//           console.log('Query result:', result.rows);
-//         }
-//       });
-//     };
-//   });
+    clickMe.onclick = function testConnection() {
+      pool.query('SELECT * FROM Supplier', (err, result) => {
+        if (err) {
+          console.error('Error executing query:', err);
+        } else {
+          console.log('Query result:', result.rows);
+        }
+      });
+    };
+  });
+
+  
