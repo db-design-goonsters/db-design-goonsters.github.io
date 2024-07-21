cc14f6d3900cec3105fba44a47831ebc1a21ee84
o-cronin <79036835+o-cronin@users.noreply.github.com>
2024-07-21 16:16:29 -0400

    push

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,7 @@ import { pool } from "./services/pool";
 const clickMe = document.querySelector('.intro .test-connection');
 
 clickMe.onclick = function testConnection() {
-    pool.query(`insert into supplier
-        ( suppId, suppName, supplierAddress ) VALUES
-          (4,'giant eagle','america'), 
-          (5,'chewy','puppy land'), 
-          (6,'taj mahal','india'); `, (err, result) => 
+    pool.query(`Select * From Supplier`, (err, result) => 
           {
   if (err) { 
       console.error('Error executing query:', err); 
