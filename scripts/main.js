8dc1ad1312fb2ebbb9bcbddd2e09678f4e2450c8
o-cronin <79036835+o-cronin@users.noreply.github.com>
2024-07-21 21:52:03 -0400

    old push

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,26 @@
-import { pool } from "./services/pool";
+// const { Pool } = require('pg'); 
+
+import { Pool } from 'pg';
+
+const pool = 
+new Pool({ user: 'postgres', 
+host: 'localhost', 
+database: 'postgres', 
+password: 'your_password', 
+port: '5432', 
+max: 20, // Maximum number of clients in the pool 
+idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed 
+}); 
 
 // const clickMe = document.querySelector('.intro .test-connection');
+console.log('im in the main.js file!')
 
 document.addEventListener('DOMContentLoaded', () => {
     const clickMe = document.querySelector('.intro .test-connection');
   
     clickMe.onclick = function testConnection() {
-      pool.query('SELECT * FROM Supplier', (err, result) => {
+        console.log('you clicked the button!')
+      pool.query('SELECT * FROM supplier', (err, result) => {
         if (err) {
           console.error('Error executing query:', err);
         } else {
